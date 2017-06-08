ba16187a6a7d0e8912be0dd0c1b7f0a188cf740d
pankajtandon <pankajtandon@gmail.com>
2017-06-07 21:23:11 -0400

    added authorities

--- src/main/java/com/nayidisha/sentinel/config/CustomTokenEnhancer.java
+++ src/main/java/com/nayidisha/sentinel/config/CustomTokenEnhancer.java
@@ -8,12 +8,16 @@ import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CustomTokenEnhancer implements TokenEnhancer {
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
         final Map<String, Object> additionalInfo = new HashMap<>();
-        additionalInfo.put("SOME_ARBIT_INFO", authentication.getName() + " " + LocalDate.now().toString());
+        additionalInfo.put("USER_INFO", authentication.getName() + " " + LocalDate.now().toString());
+        additionalInfo.put("expires_in", 360000);
+        additionalInfo.put("permissions",
+                authentication.getAuthorities().stream().map(e -> toString()).collect(Collectors.toList()));
         ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
         return accessToken;
     }
